751b9a93db735d361af1876d695bba9e0edac181
ddubidubap <park62000@gmail.com>
2022-03-03 16:31:23 +0900

    third commit

--- app/src/main/java/com/example/picturediary/CalendarActivity.kt
+++ app/src/main/java/com/example/picturediary/CalendarActivity.kt
@@ -1,9 +1,16 @@
 // 참고 : https://linuxtut.com/en/0993c489e7ef1c0f969d/
 package com.example.picturediary
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.calendar.*
+import android.widget.Toast
+
+import android.widget.AdapterView.OnItemClickListener
+import android.widget.AdapterView
+import kotlinx.android.synthetic.main.calendar_cell.*
+
 
 class CalendarActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,5 +29,12 @@ class CalendarActivity : AppCompatActivity() {
         }
         calendarGridView.adapter = mCalendarAdapter
         titleText.text = mCalendarAdapter.getTitle()
+
+        calendarGridView.setOnItemClickListener { parent, view, position, id ->
+            val element = mCalendarAdapter.getItemId(position)
+            println(element)
+            val intent = Intent(this, DrawingActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/picturediary/DrawingActivity.kt
@@ -0,0 +1,11 @@
+package com.example.picturediary
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DrawingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.drawing_main)
+    }
+}
