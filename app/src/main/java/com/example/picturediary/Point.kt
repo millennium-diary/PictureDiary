1910fa7645b1a954a8753d40970fffe24b5a1f0b
ddubidubap <park62000@gmail.com>
2022-04-07 19:33:23 +0900

    로그인, 올가미, 그룹 병합

---
+++ app/src/main/java/com/example/picturediary/Point.kt
@@ -0,0 +1,11 @@
+package com.example.picturediary
+
+class Point {
+    var dy = 0f
+    var dx = 0f
+    var x = 0f
+    var y = 0f
+    override fun toString(): String {
+        return "$x, $y"
+    }
+}
