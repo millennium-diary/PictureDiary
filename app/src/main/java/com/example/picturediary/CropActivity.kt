86659e92da98eb11b034cb7377273152764e975a
LHY00y <leeha0038@gmail.com>
2022-04-26 17:16:56 +0900

    크롭>텍스트
    완료버튼누르면 이동하는 기능

--- app/src/main/java/com/example/picturediary/CropActivity.kt
+++ app/src/main/java/com/example/picturediary/CropActivity.kt
@@ -1,8 +1,11 @@
 package com.example.picturediary
 
+import android.content.Intent
 import android.graphics.*
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_crop.*
 
 
 class CropActivity: AppCompatActivity() {
@@ -21,5 +24,16 @@ class CropActivity: AppCompatActivity() {
         cropView = findViewById(R.id.crop_view)
         cropView?.setDrawId(pickedDate!!)
         cropView?.setDrawing(picture!!)
+
+        completeBtn.setOnClickListener {//완료버튼
+            val intent = Intent(this, TextActivity::class.java)
+            startActivity(intent)
+        }
+
+        playAll.setOnClickListener {//모두재생 버튼
+
+        }
+
+
     }
 }
