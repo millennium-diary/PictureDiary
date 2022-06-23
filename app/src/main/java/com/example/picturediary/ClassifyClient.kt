6b89d868c06f4f21f50d421a860dff4819b5d88b
현경 <nhk1657@kpu.ac.kr>
2022-06-23 11:54:26 +0900

    ip 설정

--- app/src/main/java/com/example/picturediary/ClassifyClient.kt
+++ app/src/main/java/com/example/picturediary/ClassifyClient.kt
@@ -11,7 +11,8 @@ import java.net.InetSocketAddress
 
 
 class ClassifyClient {
-    private val hostname = "192.168.0.2"
+    private val hostname = "192.168.181.15"
+//    private val hostname = "192.168.0.2"
 //    private val hostname = "192.168.0.44"     // TUK 와이파이
 
     private var last = false
